4bac0c75cf7918b9d1d0c8fabbd592a7e57bc921
Diego Zanella <businessdad75@gmail.com>
2014-07-01 14:46:33 +0100

    Added SQL for v_TP_ThanksLog view

--- ThankfulPeople/install/thankfulpeople.schema.php
+++ ThankfulPeople/install/thankfulpeople.schema.php
@@ -25,11 +25,47 @@ class Schema extends \Aelia\Schema {
 												 'User', array('UserID'));
 	}
 
+	/**
+	 * Alters the Users table, adding fields to keep track of thanks.
+	 */
 	protected function alter_users_table() {
 		Gdn::Structure()
 			->Table('User')
 			->Column('ReceivedThankCount', 'int', 0)
 			->Set(false, false);
+	}
+
+	/**
+	 * Creates a view to access the ThanksLog table.
+	 *
+	 * What's the purpose of this view?
+	 * This view comes useful when joining with tables that have fields with the
+	 * same names, such as Discussion.UserID. Some plugins add their own JOIN and
+	 * WHERE clauses, but they don't specify a table prefix. That is, they can add
+	 * a clauses such as "WHERE (UserID = 123)". If joining with ThanksLog, such
+	 * clause becomes ambiguous, as both Discussion and ThanksLog table contain
+	 * a UserID field, and the query fails.
+	 *
+	 * Why not renaming the ThanksLog.UserID field?
+	 * That was eveluated, but other applications and plugins already use such
+	 * field. Using a view when joining with ThanksLog allows to alias the field,
+	 * preventing the issue described above, without having to touch 3rd party
+	 * entities.
+	 */
+	protected function create_thankslog_view() {
+		$Px = $this->Px;
+		$Sql = "
+			SELECT
+				TL.`ThankID`
+				,TL.`UserID` as RecipientUserID
+				,TL.`ObjectType`
+				,TL.`ObjectID`
+				,TL.`DateInserted`
+				,TL.`InsertUserID`
+			FROM
+				{$Px}ThanksLog TL
+		";
+		$this->Construct->View('v_TP_ThanksLog', $Sql);
 	}
 
 	/**
@@ -80,8 +116,10 @@ class Schema extends \Aelia\Schema {
 	protected function CreateObjects() {
 		$this->create_thankslog_table();
 		$this->alter_users_table();
+
 		$this->create_userreceivedthanks_view();
 		$this->create_objectreceivedthanks_view();
+		$this->create_thankslog_view();
 	}
 
 	/**
@@ -90,6 +128,7 @@ class Schema extends \Aelia\Schema {
 	protected function DropObjects() {
 		$this->DropView('v_TP_UserReceivedThanks');
 		$this->DropView('v_TP_ObjectReceivedThanks');
+		$this->DropView('v_TP_ThanksLog');
 
 		$this->DropTable('ThanksLog');
 	}
