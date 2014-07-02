2be91144e47c8e177b030f358ed050aad3930c77
Diego Zanella <businessdad75@gmail.com>
2014-07-02 12:52:05 +0100

    Added UserModel::RecalculateReceivedThanksCount() method

--- ThankfulPeople/lib/classes/models/class.tp_usermodel.php
+++ ThankfulPeople/lib/classes/models/class.tp_usermodel.php
@@ -30,4 +30,27 @@ class UserModel extends \UserModel {
 			->Where('UserID', $UserID)
 			->Put();
 	}
+
+	/**
+	 * Recalculates the amount of thanks received by a User.
+	 *
+	 * @return int
+	 */
+	public function RecalculateReceivedThanksCount() {
+		$Px = $this->Px;
+		$UpdateSQL = "
+			UPDATE
+				{$Px}User U
+			LEFT JOIN
+				{$Px}v_TP_UserReceivedThanks URT ON
+					(URT.UserID = U.UserID)
+			SET
+				(U.ReceivedThanksCount = URT.ThanksCount)
+		";
+
+		$Result = $this->SQL->Query($UpdateSQL, null);
+
+		// Return the amount of affected rows
+		return $Result->PDOStatement()->rowCount();
+	}
 }
